a511ef1a172be289573036ebcb927169408e572c
legisetiawan <legi.setiawan@gmail.com>
2024-01-26 16:08:14 +0700

    membuat file matriks operasi  dan nulis code

---
+++ matriks_operasi.py
@@ -0,0 +1,7 @@
+import numpy as np
+matriks = np.array([
+    [5,0],
+    [1,-2]
+])
+perkalian = matriks * 2 # menentukan bilangan constanta 2 , sesua kebutuhan Note ; transporse matriks, inverse dll
+print(perkalian)
