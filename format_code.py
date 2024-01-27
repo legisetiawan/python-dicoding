d809b3ccfc029dbb327e62e9e3ef25205e267cbc
legisetiawan <legi.setiawan@gmail.com>
2024-01-27 11:07:33 +0700

    membuat file format code nulis code

---
+++ format_code.py
@@ -0,0 +1,12 @@
+# format code membantu menyusun code lebih rapi dan mudah di baca, yapf lansung memberikan contoh dari code yg di jalankan. tinggal memperbaiki.
+class Kalkulator:
+    """kalkulator tambah kurang"""
+    
+    def __init__(self, _i):
+        self.i = _i
+        
+    def tambah(self, _i): 
+        return self.i + _i
+    
+    def kurang(self, _i):
+        return self.i - _i
